591a12b958e5a8e1db4813d60c8a77893972515a
Dawson <horvath.dawson@gmail.com>
2021-11-18 16:02:10 -0800

    working with external core?

---
+++ bindings/Test.py
@@ -0,0 +1,6 @@
+import scenario.bindings.monopod as monopod
+
+
+world = monopod.World()
+model = world.get_model("monopod")
+print(model.joint_names())
--- bindings/__init__.py
+++
@@ -1,6 +0,0 @@
-import scenario.bindings.monopod as monopod
-
-
-world = monopod.World()
-model = world.get_model("monopod")
-print(model.joint_names())
