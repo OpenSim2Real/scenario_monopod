41951ba0be142da4430b906402dbd59e9bcdb40a
Dawson <horvath.dawson@gmail.com>
2021-11-23 14:41:47 -0800

    added FindScenario.cmake

---
+++ cmake/FindScenario.cmake
@@ -0,0 +1,15 @@
+
+execute_process(
+    COMMAND "${Python3_EXECUTABLE}" -c "import scenario; print(scenario.__path__[0])"
+    OUTPUT_VARIABLE OUT_PATH
+)
+message(STATUS ${OUT_PATH})
+
+find_library (
+	ScenarioInstalled
+	NAMES "Scenario"
+	HINTS ${OUT_PATH}
+	DOC "Scenario CPP Core library"
+)
+
+find_package(Scenario REQUIRED core HINTS OUT_PATH)
